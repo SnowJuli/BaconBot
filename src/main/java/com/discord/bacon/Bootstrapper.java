1388fd4055acc4c7362b28a155e5e8dec1409776
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-04-08 22:52:18 +0200

    initial commit

--- src/main/java/com/discord/bacon/Bootstrapper.java
+++ src/main/java/com/discord/bacon/Bootstrapper.java
@@ -1,12 +1,17 @@
 package com.discord.bacon;
 
 import com.discord.bacon.bot.BaconBot;
+import com.github.kaktushose.jda.commands.entities.JDACommands;
+import com.github.kaktushose.jda.commands.entities.JDACommandsBuilder;
+import de.kaktushose.discord.reactionwaiter.ReactionListener;
 
 public class Bootstrapper {
 
     public static void main(String[] args) {
         BaconBot baconBot = new BaconBot("");
         baconBot.start();
+        ReactionListener.startListening(baconBot.getJda());
+        JDACommands jdaCommands = new JDACommandsBuilder(baconBot.getJda()).addProvider(baconBot).setCommandPackage("com.discord.bacon.bot").build();
     }
 
 }
---
+++ src/main/java/com/discord/bacon/bot/SubCommand.java
@@ -0,0 +1,25 @@
+import com.github.kaktushose.jda.commands.annotations.*;
+import com.github.kaktushose.jda.commands.api.EmbedCache;
+import com.github.kaktushose.jda.commands.entities.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+
+import java.util.concurrent.TimeUnit;
+
+@CommandController("sub")
+public class SubCommand {
+
+    @Inject EmbedCache embedCache;
+
+    @Command("greet")
+    public void onSubGreet(CommandEvent event, @Optional Member member) {
+        event.reply("Hello %s!", member == null ? "World" : member.getAsMention());
+    }
+
+    @Command("reactionmsg")
+    public void onTellAge(CommandEvent event, @Concat String msg) {
+        event.reply(embedCache.getEmbed("reactionmsg"));
+    }
+
+
+}
