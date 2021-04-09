2c4acdec5874f3f430248cd4e75c05708c25d756
Leo <49914615+SnowLeoo@users.noreply.github.com>
2021-04-09 14:49:38 +0200

    started with scraper

--- Eggs/index.py
+++ Eggs/index.py
@@ -1,1 +1,15 @@
-exit("Not implemented")
+# Imports 
+import PyInquirer
+from instascrape import *
+from selenium import webdriver
+
+# Configuration
+driver = webdriver.Firefox()
+target = 'https://www.instagram.com/kinky_bacon/'
+insta_profile = Profile(target)
+
+# Query
+insta_profile.scrape()
+posts = insta_profile.get_posts(driver, amount=2, login_first=True, scrape=True, scrape_pause=5)
+
+print(posts)
